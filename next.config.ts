ff8165038ac79bce83b11eb759f8ffb5b41f64f3
suryakant subudhi <suryakant.subudhi@outlook.com>
2026-06-22 12:17:08 +0530

    fix: eve runtime error

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import { withEve } from "eve/next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["eve"],
+  // serverExternalPackages: ["eve"],
   images: {
     remotePatterns: [
       {
